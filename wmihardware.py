5849f06edf26e2ee1bfa4363bd8096aa1c4298d6
Bogdan <86776650+Bogdan0255@users.noreply.github.com>
2022-10-27 09:42:14 +0300

    Update wmihardware.py

--- wmihardware.py
+++ wmihardware.py
@@ -2,6 +2,9 @@ import platform
 
 
 print (f"Architecture: {platform.architecture()}")
+print(f"Platform processor:', {platform.processor()}")
 print (f"Network name: {platform.node()}")
 print (f"Operating System: {platform.platform()}")
-
+print(f"Machine type: {platform.machine()}")
+print(f"Python build no. and date: {platform.python_build()}")
+print(f"Python compiler:', {platform.python_compiler()}")
